98e68693882f7df038346fdf7f0d0ca566b4868c
Anna Doshchenko <annadoshenko3@gmail.com>
2021-12-17 01:10:51 +0300

    0 Task completed

---
+++ 0_task.cpp
@@ -0,0 +1,37 @@
+
+   
+#include <iostream>
+#include<string.h>
+
+using namespace std;
+
+
+int main(int argc, char** argv)
+{
+       if (argc > 1) // if we pass arguments to function, argc will be more than 1 (depends on number of arguments)
+       {
+          int numWords = 0; // variable for counting number of words
+          // check if first symbol is not empty 
+          if (!(isspace(argv[1][0]) || argv[1][0] == '\n')){
+             numWords++;
+          }
+          cout << strlen(argv[1]) << endl; 
+          for(int i = 1; i < strlen(argv[1]); i++){
+             if (
+                // hceck if symbol is space or newline
+                  (isspace(argv[1][i]) || argv[1][i] == '\n')  
+                  && 
+                  // check that next symbol is not space and not newline
+                  !(isspace(argv[1][i + 1]) || argv[1][i + 1] == '\n')
+                ){
+                numWords++;
+             } 
+          };
+             printf("Number of words in %s = %d\n", argv[1], numWords); // printing number of words
+       }
+       else
+      {
+         cout << "Not arguments were given" << endl;
+      }
+       return 0;
+}
